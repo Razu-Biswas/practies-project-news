6c87891adac0a30c5ffa1c0dfb4a9439e6f53310
Razu-Biswas <sreekantaraju3421@gmail.com>
2021-03-09 04:00:57 +0600

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import News from './Components/News/News.js';
+import Header from './Components/Header/Header';
+import TopHeadline from './Components/TopHeadline/TopHeadline.js';
+import Fragment from './Components/Fragment/Fragment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>React Bootstrap and Material UI</h3>
+      <Header />
+      <Fragment />
+      <TopHeadline />
+
     </div>
   );
 }
---
+++ src/Components/Fragment/Fragment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fragment = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default Fragment;
